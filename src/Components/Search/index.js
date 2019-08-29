6cfd97f6bdc03df3b0e5d202c10e1578a5fcd227
brellin <cekakid@gmail.com>
2019-08-29 02:14:53 -0500

    ANNNAAAAAA

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import Popup from "./popup";
 
 export default class Search extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             isPopupOpen: false,
@@ -14,64 +14,63 @@ export default class Search extends React.Component {
     }
 
     async fetchData() {
-        const sports= await fetch("/").catch((err) => {
-this.setError("Cannot load data from server")
+        const sports = await fetch("/").catch((err) => {
+            this.setError("Cannot load data from server")
             //todo: handle errors
         })
-        this.setState({ sports: (await sports)})
+        this.setState({ sports: (await sports) })
     }
 
     componentDidMount() {
-this.fetchData();
+        this.fetchData();
     }
 
-    searchSports(keyword){
-const { sports } = this.state;
+    searchSports(keyword) {
+        const { sports } = this.state;
     }
 
     onInputChange(e) {
-const keyword = e.target.value;
-this.searchSports(keyword)
+        const keyword = e.target.value;
+        this.searchSports(keyword)
     }
 
 
     onInput(e) {
-if (e.target.value !== "") this.showPopup();
-else this.hidePopup();
-//show up popup
+        if (e.target.value !== "") this.showPopup();
+        else this.hidePopup();
+        //show up popup
+    }
+    showPopup = () => {
+        this.setState({ isPopupOpen: true });
+    }
 
-showPopup(){
-this.setState({isPopupOpen:true});
-}
+    hidePopup = () => {
+        this.setState({ isPopupOpen: false });
+    }
 
-hidePopup(){
-    this.setState({isPopupOpen:false});
-}
+    SetError = (msg) => {
+        this.setState((prevState) => ({ errors: [...prevState.errors, msg], isError: true }));
+    }
 
-SetError(msg) {
-    this.setState((prevState) => ({errors: [...prevState.errors, msg], isError: true}));
-}
-
-clearError(msg) {
-    this.setState({errors: [], isError: false});
-}
+    clearError = (msg) => {
+        this.setState({ errors: [], isError: false });
     }
 
     render() {
         const { isPopupOpen } = this.state;
-        return(
+        return (
             <div className="search">
-            <div className="search-container">
-            <div className="title">Need Help?</div>
-            <div className="content">
-            <input type="text" placeholder="SportsSearch" onInput={this.onInput.bind(this)} onChange={this.onInputChange.bind()}></input>
-            <Popup
-            isOpen={isPopupOpen
-            items={[{name: "sports"}, {name: "sports"}]}
-            />
-            </div>
-            </div>
+                <div className="search-container">
+                    <div className="title">Need Help?</div>
+                    <div className="content">
+                        <input type="text" placeholder="SportsSearch" onInput={this.onInput.bind(this)} onChange={this.onInputChange.bind()}></input>
+                        <Popup
+                            isOpen={isPopupOpen}
+                            items={[{ name: "sports" }, { name: "sports" }]}
+                        />
+                    </div>
+                </div>
             </div>
         )
     }
-    }
+}
