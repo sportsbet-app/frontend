2a32467d666e08e418d7fc1a9a92101a40f201f3
brellin <cekakid@gmail.com>
2019-08-28 23:08:54 -0500

    groupings made

--- src/Components/Sports/MainComp/NFLTeam.js
+++ src/Components/Sports/MainComp/NFLTeam.js
@@ -1,48 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from 'styled-components'
-import axios from 'axios';
+import { connect } from 'react-redux'
 
-import NFL from "../../../Assets/urls"
 import TeamContent from "./TeamContent";
 
 const Container = styled.div`
     max-width: 100%;
 `
 
-const NFLTeam = () => {
-    const [teams, setTeams] = useState([])
+const NFLTeam = props => {
 
-    useEffect(() => {
-        const getInfo = () => {
-            axios
-                .get('https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4391')
-                .then(response => {
-                    setTeams(response.data);
+    if (!props.teams) return <Container>Loading</Container>
 
-                })
-                .catch(error => {
-                    console.error('Server Error', error);
-                });
-        }
-
-        getInfo();
-        console.log(teams)
-    }, []);
-
-    const toggle = () => {
-        if (teams.length !== 0) {
-            return teams.teams.map(el => <TeamContent team={el} />)
-        } else {
-            return <div>loading</div>
-        }
-    }
-
-    return (
-        <Container>
-            {toggle()}
-        </Container>
-    )
+    else return <Container>
+        {props.teams.map((pair, id) => <TeamContent key={id} team={pair} />)}
+    </Container>
 
 }
 
-export default NFLTeam;
+const mapStateToProps = state => ({ teams: state.nflTeamData })
+
+export default connect(mapStateToProps)(NFLTeam);
--- src/Components/Sports/MainComp/TeamContent.js
+++ src/Components/Sports/MainComp/TeamContent.js
@@ -3,6 +3,12 @@ import axios from "axios";
 
 import styled from 'styled-components'
 
+const Grouping = styled.div`
+    max-width: 50%;
+    margin: 50px 0;
+    border: 5px solid red;
+`
+
 const TeamLogo = styled.img`
     max-width: 100%;
 `
@@ -11,14 +17,20 @@ const TeamContent = (props) => {
     //This component Takes in props from to display the information for the teams.
 
     return (
-        <div>
+        <Grouping>
             <div>
                 <div >
-                    <TeamLogo src={props.team.strTeamLogo} alt="" />
+                    <TeamLogo src={props.team.first.strTeamLogo} alt="" />
                 </div>
-                {props.team.strTeam}
+                {props.team.first.strTeam}
             </div>
-        </div>
+            <div>
+                <div >
+                    <TeamLogo src={props.team.second.strTeamLogo} alt="" />
+                </div>
+                {props.team.second.strTeam}
+            </div>
+        </Grouping>
     );
 }
 
