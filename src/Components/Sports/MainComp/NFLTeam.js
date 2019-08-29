f589f179912fd4321f970d3c0b9ae6c4cf6d13af
arodri04 <augustine.rodriguez55@gmail.com>
2019-08-28 17:08:46 -0700

    Added teams to main page, all info is there

--- src/Components/Sports/MainComp/NFLTeam.js
+++ src/Components/Sports/MainComp/NFLTeam.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import NFL from "../../../Assets/urls"
+import TeamContent from "./TeamContent";
+
+
 
 const NFLTeam = () => {
+    const [teams, setTeams] = useState([])
 
+    useEffect(() => {
+        const getInfo = () => {
+            axios
+                .get('https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4391')
+                .then(response => {
+                    setTeams(response.data);
+
+                })
+                .catch(error => {
+                    console.error('Server Error', error);
+                });
+        }
+
+        getInfo();
+        console.log(teams)
+    }, []);
+
+    const toggle= () => {
+        if (teams.length !== 0 ) {
+            return teams.teams.map(el => <TeamContent team={el}/>)
+        } else {
+            return <div>loading</div>
+        }
+    }
+    
     return(
-        <div>NFL Teams</div>
+        <div>
+            {toggle()}
+        </div>
     )
 
 }
