6926a036f8e52f6a50a92cc9e30135bef22bb3a7
brellin <cekakid@gmail.com>
2019-08-30 00:02:34 -0500

    player search

--- src/Components/Sports/MainComp/NFLPlayers.js
+++ src/Components/Sports/MainComp/NFLPlayers.js
@@ -1,17 +1,12 @@
-import React from "react";
-import Loader from 'react-loader-spinner'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
-
+import React, { useState } from "react";
 import styled from 'styled-components'
-import { colors, buttonSize } from '../../../Theme/Variables'
 
+import { colors, buttonSize } from '../../../Theme/Variables'
 import { playerOptions } from '../../../Assets/DummyData'
 import PlayerCard from './PlayerCard'
 
 const Container = styled.div`
     width: 100%;
-    border: 2px solid cyan;
     display: flex;
     flex-wrap: wrap;
 `
@@ -22,44 +17,57 @@ const LoaderContainer = styled.div`
 `
 
 const SearchContainer = styled.div`
-width: 100%;
-margin: 4rem 2rem;
+    width: 100%;
+    margin: 4rem 2rem;
 
 
-input {
-  width: 500px;
-  border: 0;
-  height: 40px;
-  background: ${colors.secondary};
-  font-size: 1.4rem;
-  color: #fff;
-  border-bottom: 2px dotted ${colors.darkGrey};
-
-  ::placeholder,
-  ::-webkit-input-placeholder {
+    input {
+    width: 500px;
+    border: 0;
+    height: 40px;
+    background: ${colors.secondary};
+    font-size: 1.4rem;
     color: #fff;
-  }
-  :-ms-input-placeholder {
-    color: #fff;
-  }
+    border-bottom: 2px dotted ${colors.darkGrey};
+    outline: none;
+
+    ::placeholder,
+    ::-webkit-input-placeholder {
+        color: #fff;
+    }
+    :-ms-input-placeholder {
+        color: #fff;
+    }
 }
 `
 
+{/* <LoaderContainer>
+    <Loader
+        type="ThreeDots"
+        color={colors.primary}
+        height={80}
+        width={80}
+    />
+</LoaderContainer> */}
+
 const NFLPlayers = () => {
+
+    const [search, setSearch] = useState('')
 
     return (
 
         <Container>
-            {/* <LoaderContainer>
-                <Loader
-                    type="ThreeDots"
-                    color={colors.primary}
-                    height={80}
-                    width={80}
+            <SearchContainer>
+                <input
+                    type="text"
+                    placeholder="Enter Player Name To Search Players"
+                    onChange={e => setSearch(e.target.value)}
                 />
-            </LoaderContainer> */}
-            <SearchContainer><input type="text" placeholder="Enter Player Name To Search Players" /></SearchContainer>
-            {playerOptions.map(player => <PlayerCard player={player} />)}
+            </SearchContainer>
+            {search.length ?
+                playerOptions.filter(player => player.name.toLowerCase().includes(search.toLowerCase())).map(player => <PlayerCard player={player} />)
+                :
+                playerOptions.map(player => <PlayerCard player={player} />)}
         </Container>
 
     )
