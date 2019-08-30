9bf737399327e464586da33bcb37b8d0a09c8c32
Adetunji <ashennaike@gmail.com>
2019-08-30 05:42:54 -0400

    comments on favorites and test starting to form, same issue as betslip where connect is throwing an error for the test and have to figure out how to implement provider into the test.

--- src/Components/Favorites/Favorites.js
+++ src/Components/Favorites/Favorites.js
@@ -1,44 +1,44 @@
 import React from 'react'
+//import connect for the redux state grab
 import { connect } from 'react-redux'
-
+//import styling
 import styled from 'styled-components'
 import { colors, buttonSize } from '../../Theme/Variables'
 
 const FavoritesContainer = styled.div`
-    width: 95%;
-    padding: .5rem .5rem;
-    margin: 0 auto;
-    margin-top: 20px;
-    background: ${colors.secondary};
-    color: #fff;
+  width: 95%;
+  padding: .5rem .5rem;
+  margin: 0 auto;
+  margin-top: 20px;
+  background: ${colors.secondary};
+  color: #fff;
 
-    p {
-        font-size: 1.2rem;
-    }
+  p {
+      font-size: 1.2rem;
+  }
 `
 
-
+//function grabbing its props from redux global state
 const Favorites = (props) => {
-
-    console.log(props.favoriteList)
-
-    if (!props.favoriteList.length) return (
-        <FavoritesContainer>
-            <p>You don't have any favorites selected yet. Please click that badass star that Brandi made to make  a team be a favorite of yours.</p>
-        </FavoritesContainer>
-    )
-
-    return (
-        <FavoritesContainer>
-            {props.favoriteList.map(fav => <Favorite teamOne={fav.first} teamTwo={fav.second} />)}
-        </FavoritesContainer>
-    )
+  console.log(props.favoriteList)
+  //if a list doesn't exists prompt the user to make one
+  if (!props.favoriteList.length) return (
+    <FavoritesContainer>
+      <p>You don't have any favorites selected yet. Please click that badass star that Brandi made to make  a team be a favorite of yours.</p>
+    </FavoritesContainer>
+  )
+  //otherwise display the list
+  return (
+    <FavoritesContainer>
+      {props.favoriteList.map(fav => <Favorite teamOne={fav.first} teamTwo={fav.second} />)}
+    </FavoritesContainer>
+  )
 }
-
+//items to be displayed in function above
 function Favorite(props) {
-    return <p>{`${props.teamOne.strTeam} VERSUS ${props.teamTwo.strTeam}`}</p>
+  return <p>{`${props.teamOne.strTeam} VERSUS ${props.teamTwo.strTeam}`}</p>
 }
 
 const mapStateToProps = state => ({ ...state })
-
+//connecting only the global state no functions
 export default connect(mapStateToProps)(Favorites)
--- src/Components/Favorites/Favorites.spec.js
+++ src/Components/Favorites/Favorites.spec.js
@@ -1,26 +1,36 @@
 import React from 'react'
 //import dependencies for tests
 import { render } from '@testing-library/react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { useAuth0 } from "./Utils/authZero"
+import { Provider } from 'react-redux'
 
 //import renderer for snapshot
 import renderer from 'react-test-renderer'
 //import component
-import App from './App'
+import FavoritesContainer from './Favorites'
+
+//test data to use for all test
+const data = [
+  {first: 'a team',
+  teamOne: {strTeam: 'first image'},
+  second: 'another team',
+  teamTwo: {strTeam: 'some image'}}
+]
 
 //test to check that the app will display when called.
-describe('<App />', () => {
+describe('<FavoritesContainer />', () => {
   it('should display', () => {
     //render the component
-    render(<Router><App/></Router>)
+    render(<Provider><FavoritesContainer/></Provider>)
     
   })
+
   //create a snapshot
   it('matches snapshot', () => {
-    const tree = renderer.create(<Router><App/></Router>); // generates a DOM tree
+    const tree = renderer.create(<Provider><FavoritesContainer/></Provider>); // generates a DOM tree
 
     // snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
   });
+
+  //data displays properly
 })
